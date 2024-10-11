891c588e87e33db7ac26ff8c7ba7047150bdc93b
Misbah Tahir <148400107+Misbah315100@users.noreply.github.com>
2024-10-11 09:16:16 +0100

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,18 @@
+#Find out your grade
+score = input("Enter Score: ")
+
+
+if int(score) >= 90:
+    print("A++ Girlies a genius")
+elif int(score) >= 80: 
+    print("A+ Girl you Ate and left no crumbs")
+elif int(score) >= 70:
+        print("A. Girl you must've studied at the uni of serving")
+elif int(score) >= 60: 
+            print("B. Beauty with brains ugh love it")
+elif int(score) >= 50:
+                print("C, you made it bestie - what a relief")
+else:
+                    print("Fail. Dw bbg you can just marry a rich man")
+        
+                    
